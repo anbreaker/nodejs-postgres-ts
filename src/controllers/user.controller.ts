83ef0b8120f1deb669f016bd38fe146095ac36a5
anbreaker <antunez19@gmail.com>
2021-12-24 09:54:22 +0100

    basic crud ts complete

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -21,9 +21,72 @@ export const getUserById = async (req: Request, res: Response): Promise<Response
   try {
     const { id } = req.params;
 
-    const user = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    const user: QueryResult = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
 
     return res.json(user.rows);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json('Internal Server Error');
+  }
+};
+
+export const createUser = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { name, email } = req.body;
+
+    const newUser: QueryResult = await pool.query(
+      'INSERT INTO users (name, email) VALUES ($1, $2)',
+      [name, email]
+    );
+
+    return res.json({
+      message: 'User Create',
+      user: {
+        name,
+        email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json('Internal Server Error');
+  }
+};
+
+export const updateUser = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    const updateUser: QueryResult = await pool.query(
+      'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+      [name, email, id]
+    );
+
+    return res.json({
+      message: `User with id ${id} Updated`,
+      user: {
+        id,
+        name,
+        email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json('Internal Server Error');
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { id } = req.params;
+    const deleteUser: QueryResult = await pool.query('DELETE FROM users WHERE id = $1', [
+      id,
+    ]);
+
+    return res.json({ message: `User ${id} deleted` });
   } catch (error) {
     console.log(error);
 
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
 const router = Router();
 
-import { getUsers, getUserById } from '../controllers/user.controller';
+import {
+  createUser,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+} from '../controllers/user.controller';
 
 // /api/users
 router.get('/users', getUsers);
-// router.post('/users', getUser);
+router.post('/users', createUser);
 
 // /api/users/:id
 router.get('/users/:id', getUserById);
-// router.put('/users/:id', updateUser);
-// router.delete('/users/:id', deleteteUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
